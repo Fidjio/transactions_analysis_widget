3d7649128c31d8ad69c80cff61ba839196becfe8
Сергей Карсканов <kapckahob97@gmail.com>
2025-04-07 22:24:08 +0300

    add func filter_by_date in utils.py

--- src/utils.py
+++ src/utils.py
@@ -45,3 +45,35 @@ def open_xlsx_file(path: str) -> DataFrame | list[Any]:
     except Exception as ex:
         logger.error(f"Ошибка при открытии файла: {ex}")
         return []
+
+
+def filter_by_date(df_transactions: DataFrame, user_date: str) -> list[Any] | Any:
+    """Фильтрует транзакции по конечной дате (включая user_date)"""
+    try:
+        # Создаем копию DataFrame, чтобы не изменять исходные данные
+        df_transactions = df_transactions.copy()
+        logger.info("Преобразование столбца с датой полученного DataFrame..")
+        # Преобразуем столбец с датой в datetime и устанавливаем его как индекс
+        df_transactions['Дата операции'] = pd.to_datetime(df_transactions['Дата операции'],
+                                                          dayfirst=True, format="%d.%m.%Y %H:%M:%S")
+        df_transactions = df_transactions.set_index('Дата операции')
+
+        logger.info("Сортировка индекса (индекс столбец с датами)")
+        # Сортируем индекс для корректной работы срезов
+        df_transactions = df_transactions.sort_index()
+
+        logger.info("Преобразование полученной даты от пользователя в datetime")
+        # Преобразуем user_date в datetime
+        upper_date = pd.to_datetime(user_date, dayfirst=True, format="%d.%m.%Y %H:%M:%S")
+
+        # Определение начальной даты для фильтрации
+        start_date = upper_date.replace(day=1)  # 1-е число того же месяца
+
+        logger.info("Фильтрация транзакций по диапазону от начала месяца до указанной даты")
+        # Фильтруем транзакции от start_date до upper_date (включительно)
+        filtered_df = df_transactions.loc[start_date:upper_date]
+        return filtered_df
+
+    except Exception as ex:
+        logger.error(f"Ошибка при фильтрации по дате: {ex}")
+        return []
